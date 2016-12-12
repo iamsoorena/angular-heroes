4d605722fe793fdba3ea78073ad396be7c33c8fa
Soorena <s.soorena@gmail.com>
2016-12-12 18:27:29 +0330

    We created a reusable component We learned how to make a component accept input We learned to declare the application directives we need in an Angular module. We list the directives in the NgModule decorator's declarations array. We learned to bind a parent component to a child component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import {Hero} from "./hero-detail/hero";
 
 @Component({
   selector: 'app-root',
@@ -14,19 +10,13 @@ export class Hero {
         <li 
           *ngFor="let hero of heroes" 
           (click)="onSelect(hero)" 
-          [class.selected]="hero === selectedHero"
+          [class.selected]="hero === hero"
          >
           <span class="badge">{{hero.id}}</span> {{hero.name}}
         </li>
       </ul>
-      <div *ngIf="selectedHero">
-        <h2>{{selectedHero.name}} details!</h2>
-        <div><label>id: </label>{{selectedHero.id}}</div>
-        <div>
-            <label>name: </label>
-            <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-        </div>
-      </div>
+      <app-hero-detail [hero]="selectedHero"></app-hero-detail>
+      
 
       
 `,

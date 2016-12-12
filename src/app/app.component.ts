1021925025021e81db6cffc0d8d17375f2389453
Soorena <s.soorena@gmail.com>
2016-12-12 15:59:17 +0330

    adding to variables and changing the content off template to show them.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
+  template: `
+      <h1>{{title}}</h1>
+      <h2>{{hero}} details!</h2>
+`,
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'Soorena works!';
+  title = 'Tour of Heroes';
+  hero = 'windstorm';
 }

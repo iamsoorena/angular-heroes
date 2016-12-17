111a32646dc37377eb6590a8c3f533c0077e2ee8
Soorena <s.soorena@gmail.com>
2016-12-17 21:34:57 +0330

    We added the necessary dependencies to use HTTP in our application. We refactored HeroService to load heroes from a web API. We extended HeroService to support post, put and delete methods. We updated our components to allow adding, editing and deleting of heroes. We configured an in-memory web API. We learned how to use Observables.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import './rxjs-extensions';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
@@ -9,19 +10,24 @@ import {HeroService} from "./hero.service";
 import {AppComponent} from './app.component';
 import {DashboardComponent} from "./dashboard.component";
 import {AppRoutingModule} from "./app-routing.module";
+import {InMemoryWebApiModule} from 'angular2-in-memory-web-api';
+import {InMemoryDataService}  from './in-memory-data.service';
+import {HeroSearchComponent} from "./hero-search.component";
 
 @NgModule({
   declarations: [
     HeroesComponent,
     HeroDetailComponent,
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import {Hero} from "./hero-detail/hero";
 import {HeroService} from "./hero.service";
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
